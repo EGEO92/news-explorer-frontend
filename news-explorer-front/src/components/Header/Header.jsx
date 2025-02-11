4c94b45a46d682d65ceb14a99f977407e56583c9
EQUIPO <emanuel2581@gmail.com>
2025-02-10 18:34:14 -0600

    Hacer diseño movil

--- news-explorer-front/src/components/Header/Header.jsx
+++ news-explorer-front/src/components/Header/Header.jsx
@@ -18,9 +18,15 @@ function Header(props) {
 
   function handleLocation() {
     if (location.pathname === "/") {
-      console.log("Ancho===>>", innerWidth < 600);
       if (innerWidth < 600) {
-        return <MobileNav />;
+        return (
+          <MobileNav
+            isLoggedIn={props.loggedIn}
+            signOut={signOut}
+            signIn={signIn}
+            name={props.name}
+          />
+        );
       } else {
         return (
           <div className="header__wraper">
@@ -37,26 +43,44 @@ function Header(props) {
       }
     }
     if (location.pathname === "/saved-news") {
-      return (
-        <div className="header__wraper">
-          <Link to="/" className="header__text header__link">
-            Inicio
-          </Link>
-          <p className="header__text header__selected-user">
-            Artículos guardados
-          </p>
-          <button className="header__btn  header__btn-user" onClick={signOut}>
-            {props.name}
-            <img src={logOutB} alt="" />
-          </button>
-        </div>
-      );
+      if (innerWidth < 600) {
+        return (
+          <MobileNav
+            isLoggedIn={props.loggedIn}
+            signOut={signOut}
+            signIn={signIn}
+            name={props.name}
+          />
+        );
+      } else {
+        return (
+          <div className="header__wraper">
+            <Link to="/" className="header__text header__link">
+              Inicio
+            </Link>
+            <p className="header__text header__selected-user">
+              Artículos guardados
+            </p>
+            <button className="header__btn  header__btn-user" onClick={signOut}>
+              {props.name}
+              <img src={logOutB} alt="" />
+            </button>
+          </div>
+        );
+      }
     }
   }
 
   function handleMobile() {
     if (innerWidth < 600) {
-      return <MobileNav />;
+      return (
+        <MobileNav
+          isLoggedIn={props.loggedIn}
+          signOut={signOut}
+          signIn={signIn}
+          name={props.name}
+        />
+      );
     } else {
       return (
         <div className="header__wraper">
@@ -74,16 +98,7 @@ function Header(props) {
       <header className="header">
         <h2 className="header__tittle">NewsExplorer</h2>
 
-        {props.loggedIn
-          ? handleLocation()
-          : handleMobile()
-            // <div className="header__wraper">
-            //   <p className="header__text header__selected">Inicio</p>
-            //   <button className="header__btn" onClick={signIn}>
-            //     Iniciar sesion
-            //   </button>
-            // </div>
-        }
+        {props.loggedIn ? handleLocation() : handleMobile()}
       </header>
     </>
   );
